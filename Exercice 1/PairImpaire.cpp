75511d5d565ba55dfd91eda42787367f7f3ce49d
Zachary GAGNON <1681996@cstj.qc.ca>
2021-09-02 14:12:49 -0400

    correction exercice 2

--- Exercice 1/PairImpaire.cpp
+++ Exercice 1/PairImpaire.cpp
@@ -7,15 +7,14 @@
 void main()
 {
 	int Nombre1; // Nombre entré par l'utilisateur
-	int Modulo; // Reste de la division
+	
 
-	std::cout << "Entrer un nombre entier:";
+	std::cout << "Entrer un nombre entier:"; //Demander a l'utilisateur d'entrer un nombre entier
 	std::cin >> Nombre1;
 	//std::cout << Nombre1  //Ligne pour verifier code
 
-	Modulo = Nombre1 % 2;
-
-	if (Modulo == 0)
+	
+	if (Nombre1 %2 == 0)
 	{
 		std::cout << Nombre1 << " est un nombre pair";
 	}
--- Exercice 2/Vertical.cpp
+++ Exercice 2/Vertical.cpp
@@ -1,27 +1,46 @@
-//But: Ecrire un programme qui v#rifie si un nombre est constitu# de 5 chiffres
+//But: Ecrire un programme qui vérifie si un nombre est constitué de 5 chiffres et affice celui ci a la verticale si il a biens 5 chiffres
 //Auteur: Zachary GAGNON
 //date: 2021-02-08
 #include<iostream>
+
 using namespace std;
-void main()
+int main()
 {
+	setlocale(LC_ALL, "");
 	int number; // nombre entrer par l'utilisateur
-	cout << "Entrer un nombre a cinq chiffres :";
+
+	cout << "Entrer un nombre a cinq chiffres :"; //Demander a l'utilisateur d'inscrire un nombre
 	cin >> number;
 
-
-
 	if (number >= 10000 && number <= 99999) // si le nombre est plus haut ou egal a 10000
-											// ou si le nombre est plus petit ou egal a 99999 le code affiche le nombre sous forme verticale
-	{
-		cout << number;
+														// ou si le nombre est plus petit ou egal a 99999 le code affiche le nombre sous forme verticale
+	{                        //Affichage de chaques nombre a la verticale
+		cout << number / 10000 % 10 << endl;
+		cout << number / 1000 % 10 << endl;
+		cout << number / 100 % 10 << endl;
+		cout << number / 10 % 10 << endl;
+		cout << number % 10 << endl;
 	}
-	else                                   //sinon le code affiche ce message
-	{
-		cout << "Ce nombre ne contiens pas 5 chiffres";
+	else if (number <= -10000 && number >= -99999) //si le nombre est entre -99999 et -100000  
+
+	{                        //Affichage de chaques nombre a la verticale
+		cout << number / 10000 % 10 << endl;
+		cout << number / 1000 % 10 << endl;
+		cout << number / 100 % 10 << endl;
+		cout << number / 10 % 10 << endl;
+		cout << number % 10 << endl;
+		
 	}
+	else      //sinon le code affiche ce message
+	{
+		cout << "Ce nombre ne contiens pas 5 chiffres" << endl;
+	}
+	return 0;
 }
 // test : 12345 nombre afficher
 //        123456 message afficher
-//        99999 nombre afficher 
-//        9999 message afficher
+//        1234 message afficher
+//        -12345 nombre afficher
+//        -1234 message afficher
+//        -123456 message afficher
+//        Lettre message afficher
